57c4dca038289f1f808301d671cb8d079ddb272a
Ruben Remelgado <remelgado.ruben@gmail.com>
2020-06-04 14:40:11 +0000

    version 0.0.2

--- inst/doc/CAWaR.R
+++ inst/doc/CAWaR.R
@@ -1,10 +1,10 @@
-## ----setup, include = FALSE----------------------------------------------
+## ----setup, include = FALSE---------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
 
-## ----echo=FALSE, message=FALSE, warning=FALSE----------------------------
+## ----echo=FALSE, message=FALSE, warning=FALSE---------------------------------
 # load packages
 library(fieldRS)
 library(raster)
@@ -13,27 +13,27 @@ library(knitr)
 library(kableExtra)
 library(CAWaR)
 
-## ----message=FALSE-------------------------------------------------------
+## ----message=FALSE------------------------------------------------------------
 ndvi.ts <- brick(system.file("extdata", "ndvi.tif", package="fieldRS")) # NDVI raster time series
 data(fieldData) # ground truth data
 data(referenceProfiles) # target crop types NDVI profiles
 
-## ----echo=FALSE----------------------------------------------------------
+## ----echo=FALSE---------------------------------------------------------------
 ndvi.ts <- extend(ndvi.ts, 30)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 sampleTest <- checkSamples(fieldData)
 
 ## ---- out.width="98%", fig.height=5, fig.width=10, dpi=600, fig.align="center", fig.show='hold', echo=FALSE----
 kable_styling(kable(head(sampleTest, 1), format="html", align="c", full_width=TRUE), "stripped", bootstrap_options="responsive")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 sampleTest <- geCheck(fieldData)
 
 ## ---- out.width="98%", fig.height=5, fig.width=10, dpi=600, fig.align="center", fig.show='hold', echo=FALSE----
 kable_styling(kable(head(sampleTest, 1), format="html", align="c", full_width=TRUE), "stripped", bootstrap_options="responsive")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # build polygons
 p1 <- Polygons(list(Polygon(data.frame(x=c(1, 5, 10, 2, 1), y=c(10, 9, 8, 7, 10)))), ID=1)
 p2 <- Polygons(list(Polygon(data.frame(x=c(2, 6, 5, 4, 2), y=c(10, 9, 7, 4, 10)))), ID=2)
@@ -48,7 +48,7 @@ kable_styling(kable(head(sampleTest$overlap.df, 1), format="html", align="c", fu
 plot(p)
 plot(sampleTest$overlap.shp, col="red", add=TRUE)
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 sampleCorrect <- labelCheck(fieldData$crop)
 sampleCorrect$labels # unique labels
 
@@ -56,7 +56,7 @@ sampleCorrect$labels # unique labels
 kable_styling(kable(head(sampleCorrect$label.count, 3), format="html", align="c", full_width=TRUE), "stripped", bootstrap_options="responsive")
 sampleCorrect$label.count.plot
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 sampleCorrect <- labelCheck(fieldData$crop, sampleCorrect$labels, c("wheat", "not-wheat", "not-wheat"))
 fieldData@data$crop_2 <- sampleCorrect$labels
 
@@ -67,13 +67,13 @@ sampleCorrect$label.count.plot
 ## ---- out.width="98%", fig.height=5, fig.width=10, dpi=600, fig.align="center", fig.show='hold', echo=FALSE----
 include_graphics("percentCover.jpg")
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  fieldData2 <- poly2sample(fieldData, ndvi.ts, preserve.id=TRUE)
 
-## ---- echo=FALSE---------------------------------------------------------
+## ---- echo=FALSE--------------------------------------------------------------
 data(fieldData2)
 
-## ----eval=FALSE, message=FALSE-------------------------------------------
+## ----eval=FALSE, message=FALSE------------------------------------------------
 #  fieldDataTS <- extractTS(fieldData2, ndvi.ts, fieldData2$cover, fieldData2$id)
 
 ## ---- out.width="98%", fig.height=5, fig.width=10, dpi=600, fig.align="center", fig.show='hold', echo=FALSE----
@@ -82,7 +82,7 @@ kable_styling(kable(head(fieldDataTS$pixel.info, 5), format="html", align="c", f
 kable_styling(kable(head(fieldDataTS$polygon.info, 5), format="html", align="c", full_width=TRUE), "stripped", bootstrap_options="responsive")
 kable_styling(kable(head(fieldDataTS$weighted.mean, 5), format="html", align="c", full_width=TRUE), "stripped", bootstrap_options="responsive")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 checkTS1 <- analyseTS(as.data.frame(fieldDataTS$weighted.mean), fieldData$crop_2)
 
 ## ---- out.width="98%", fig.height=5, fig.width=10, dpi=600, fig.align="center", fig.show='hold', echo=FALSE, message=FALSE----
@@ -92,12 +92,12 @@ checkTS1$plots[[2]]
 ## ---- out.width="98%", fig.height=5, fig.width=10, dpi=600, fig.align="center", fig.show='hold', echo=FALSE----
 kable_styling(kable(head(checkTS1$r2, 5), digits=c(2,2), format="html", align="c", full_width=TRUE), "stripped", bootstrap_options="responsive")
 
-## ---- eval=FALSE---------------------------------------------------------
+## ---- eval=FALSE--------------------------------------------------------------
 #  checkTS2 <- analyseTS(as.data.frame(fieldDataTS$weighted.mean), as.character(1:length(fieldData)))
 #  
 #  for (p in 1:length(fieldData)) {ggsave(checkTS2$plots[[p]], paste0("./", checkTS2$labels[p], ".png"), width=10, height=10, units="cm")}
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 # retrieve reference profiles as a data.frame
 reference.profiles <- as.data.frame(do.call(rbind, lapply(checkTS1$y.statistics, function(i) {i$median})))
 
@@ -110,23 +110,23 @@ kable_styling(kable(head(cl$label.compare, 5), digits=c(2,2), format="html", ali
 ## ---- out.width="98%", fig.height=5, fig.width=10, dpi=600, fig.align="center", fig.show='hold', echo=FALSE----
 include_graphics("spatialValidation.png")
 
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  fieldDataCluster <- splitSamples(fieldData, ndvi.ts, fieldData$crop_2, agg.radius=60)
 
-## ----echo=FALSE----------------------------------------------------------
+## ----echo=FALSE---------------------------------------------------------------
 data(fieldDataCluster)
 
 ## ---- out.width="98%", fig.height=5, fig.width=10, dpi=600, fig.align="center", fig.show='hold', echo=FALSE----
 kable_styling(kable(head(fieldDataCluster$region.frequency, 5), format="html", align="c", full_width=TRUE), "stripped", bootstrap_options="responsive")
 
-## ------------------------------------------------------------------------
+## -----------------------------------------------------------------------------
 cropVal <- phenoCropVal(as.data.frame(fieldDataTS$weighted.mean), fieldData$crop_2, fieldDataCluster$region.id)
 
 ## ---- out.width="98%", fig.height=5, fig.width=10, dpi=600, fig.align="center", fig.show='hold', echo=FALSE----
 kable_styling(kable(head(cropVal$class.accuracy, 5), digits=c(2,2), format="html", align="c", full_width=TRUE), "stripped", bootstrap_options="responsive")
 cropVal$accuracy.plot
 
-## ----eval=FALSE----------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  fieldData$validation <- as.factor(cropVal$sample.validation)
 #  spplot(fieldData["validation"])
 
